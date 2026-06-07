b9d16f992b17172c4961477ba5c9c3a15a1c5b54
gabrielbaute <gabrielbaute@gmail.com>
2026-06-07 19:05:28 -0400

    Entrypoint de la app

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'controllers/about_controller.dart';
+import 'controllers/capacitor_controller.dart';
+import 'controllers/complex_controller.dart';
+import 'controllers/complex_calc_controller.dart';
+import 'controllers/divider_controller.dart';
+import 'controllers/parallel_controller.dart';
+import 'controllers/resistor_controller.dart';
+
+import 'router/app_router.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AboutController()),
+        ChangeNotifierProvider(create: (_) => CapacitorController()),
+        ChangeNotifierProvider(create: (_) => ComplexController()),
+        ChangeNotifierProvider(create: (_) => ComplexCalcController()),
+        ChangeNotifierProvider(create: (_) => DividerController()),
+        ChangeNotifierProvider(create: (_) => ParallelController()),
+        ChangeNotifierProvider(create: (_) => ResistorController()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Usamos el constructor .router para delegar el control a go_router
+    return MaterialApp.router(
+      title: 'UNEFA Telecom Tools',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      routerConfig: AppRouter.router, // Inyección de nuestra configuración
+    );
+  }
+}
