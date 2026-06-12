7f36610d5efe038b64ef3d606446e86d44aaeb3a
gabrielbaute <gabrielbaute@gmail.com>
2026-06-12 10:15:38 -0400

    Enums para operaciones en sistemas digitales

---
+++ lib/enums/digital_enums.dart
@@ -0,0 +1,5 @@
+/// Enum para identificar los sistemas de numeración soportados en el módulo digital.
+enum NumeralSystem { binario, octal, decimal, hexadecimal }
+
+/// Enum para las operaciones aritméticas digitales básicas.
+enum DigitalOperation { add, subtract, multiply, divide }
