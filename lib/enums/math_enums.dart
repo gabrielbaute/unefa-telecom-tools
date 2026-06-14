fd5a4fd718f5d8b2d4361526c8980f2490cb1380
gabrielbaute <gabrielbaute@gmail.com>
2026-06-14 15:47:34 -0400

    Ampliamos los enums matemáticos con un enum que determina el tipo de representación de un ángulo

--- lib/enums/math_enums.dart
+++ lib/enums/math_enums.dart
@@ -3,3 +3,12 @@ enum ComplexOperation { add, subtract, multiply, divide }
 
 /// Enum para el tipo de representación de un número complejo.
 enum ComplexNumMode { rectangular, polar }
+
+/// Enum para definir el sistema de unidades angulares admitido en la suite.
+///
+/// Attributes:
+///      decimalDegrees: Grados decimales (ej. 45.5°)
+///      sexagesimal: Grados, Minutos y Segundos (DMS) (ej. 45° 30' 0")
+///      radians: Radianes (ej. π/4)
+///
+enum AngleUnit { decimalDegrees, sexagesimal, radians }
